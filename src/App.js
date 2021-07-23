f073b37affdfbaae19b6669e3a279a3284ad7a8e
Patricia Nobre <2108le@gmail.com>
2021-07-23 15:13:33 -0300

    filtering the expenses by year

--- src/App.js
+++ src/App.js
@@ -1,56 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses';
+
+//cria o array contendo varios objetos (expenses)
+const dummy_expenses = [
+
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12)
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+
+  },
+
+];
 
 // cria o componente app
 const App = () => {
 
-  //cria o array contendo varios objetos (expenses)
-  const expenses = [
-
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 799.49,
-      date: new Date(2021, 2, 12)
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-
-  ];
+  const [expenses, setExpenses] = useState(dummy_expenses);
 
   const addExpenseHandler = (expense) => {
 
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses => {
+
+      return [expense, ...prevExpenses];
+
+    });
 
   };
 
-  //retorna o HTML/JSX
-  return (
+//retorna o HTML/JSX
+return (
 
-    <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
-    </div>
+  <div>
+    <NewExpense onAddExpense={addExpenseHandler} />
+    <Expenses items={expenses} />
+  </div>
 
-  );
+);
 
 };
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFIlter'
 import './Expenses.css'
@@ -16,31 +16,31 @@ const Expenses = (props) => {
 
   };
 
+  // filtra o ano das despesas
+  const filteredExpenses = props.items.filter(expense =>{
+
+      return expense.date.getFullYear().toString() === filteredYear;
+
+  });
+
   //retorna o HTML/JSX
   return (
 
     <div className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+      
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {filteredExpenses.map(expense => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+        ))}
+
     </div>
 
   );
